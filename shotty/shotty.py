fd5bf63cb0293e3402ed867f829657f72634de72
Nofal <nofam@8c8590ad856c.ant.amazon.com>
2018-10-04 10:31:11 -0400

    commiting

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,18 +4,73 @@ import click
 session = boto3.Session(profile_name='dynamodb')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
+def filter_instances(name):
+    instances = []
+
+    if name is not None:
+        filters = [{'Name':'tag:Name', 'Values':[name]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+@click.group()
+def instances():
+    "Commands for instances"
+
+@instances.command('list')
+@click.option('--name', default=None,
+    help="Only instances with displayName (tag Name:<name>)")
+def list_instances(name):
     "List EC2 Instances"
-    for i in ec2.instances.all():
+
+    instances = filter_instances(name)
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name)))
+            i.public_dns_name,
+            tags.get('Name', '<no name>'))))
 
     return
 
+
+@instances.command('stop')
+@click.option('--name', default=None, help='Only instances with name')
+
+def stop_instances(name):
+    "Stop EC2 instances"
+
+    instances = []
+
+    instances = filter_instances(name)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+    return
+
+@instances.command('start')
+@click.option('--name', default=None, help='Only instances with name')
+
+def start_instances(name):
+    "Start EC2 instances"
+
+    instances = []
+
+    instances = filter_instances(name)
+
+    for i in instances:
+        print("Start {0}...".format(i.id))
+        i.start()
+
+    return
+
+
 if __name__ == '__main__':
-    list_instances()
+    instances()
